ebe15b91c79b1860ae597102aa1cee11ff54853f
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-02-25 02:49:15 +0100

    master

--- sudokoscript/arraymethods.js
+++ sudokoscript/arraymethods.js
@@ -1,1 +1,5 @@
 export let array = [];
+
+export function resetArray() {
+    array = new Array(9).fill(0).map(x => new Array(9).fill(0));
+}
--- sudokoscript/sodukomain.js
+++ sudokoscript/sodukomain.js
@@ -1,6 +1,8 @@
+import { resetArray } from "./arraymethods.js";
 import { addBlocks, addCells } from "./cells.js";
 
 $(() => {
+    resetArray();
     addBlocks();
     addCells();
 })
