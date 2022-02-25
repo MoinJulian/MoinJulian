223f7db4aedd442afae7759739bf80ce13ec46a1
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-02-25 15:20:48 +0100

    master

--- sudokuscript/controls.js
+++ sudokuscript/controls.js
@@ -2,6 +2,7 @@ import { resetArray } from "./arraymethods.js";
 
 export function enableControls() {
     $("#clearBtn").on("click", clearSudoku);
+    $("#soveBtn").on("click", startSolve);
 }
 
 function clearSudoku() {
