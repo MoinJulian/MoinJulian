6437ab75ec8a6a2e9063f78b6dcaad1705c25357
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-02-25 03:13:00 +0100

    master

--- sudokoscript/cells.js
+++ sudokoscript/cells.js
@@ -1,3 +1,5 @@
+import { array } from "./arraymethods.js";
+
 export function addBlocks() {
     for (let row = 0; row < 3; row++) {
         for (let col = 0; col < 3; col++) {
@@ -18,7 +20,25 @@ export function addCells() {
             $("<input type='number'>")
             .addClass("cell")
             .attr("id", `cell${row}_${col}`)
-            .appendTo(`#block${blockRow}_${blockCol}`);
+            .appendTo(`#block${blockRow}_${blockCol}`)
+            .on("input", function () {
+                let val = $(this).val();
+                if (val.length >= 2) {
+                    $(this).val(val.substring(1));
+                }
+                val = $(this).val();
+                if (val == parseInt(val) && val >= 1 && val <= 9) {
+                    array[row][col] = val;
+                } else {
+                    $(this).val("");
+                    array[row][col] = 0;
+                }
+                if (col < 8) {
+                    $(`#cell${row}_${col+1}`).focus();
+                } else if (row < 8) {
+                    $(`#cell${row+1}_${0}`).focus();
+                }
+            });
         }
     }
 }
