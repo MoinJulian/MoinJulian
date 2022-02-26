1c2f80fa7898d2e7ca4c8260e9e5398f53435893
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-02-26 20:17:47 +0100

    master

---
+++ calculatorskript/calculator.js
@@ -0,0 +1,41 @@
+function appendOperation(operation) {
+    document.getElementById('resultArea').innerHTML += operation;
+}
+function calculatResult() {
+    let container = document.getElementById('resultArea');
+    let result = eval(container.innerHTML);
+    container.innerHTML = result;
+}
+function deleteLast() {
+    let container = document.getElementById('resultArea');
+    if(container.innerHTML.endsWith(" ")) {
+        container.innerHTML = container.innerHTML.slice(0, -3);
+    } else {
+        container.innerHTML = container.innerHTML.slice(0, -1);
+    }
+}
+//Julian Hammer
+function deleteAll() {
+    let container = document.getElementById('resultArea');
+    if(container.innerHTML.endsWith(" ")) {
+        container.innerHTML = container.innerHTML.slice(0, -Infinity);
+    } else {
+        container.innerHTML = container.innerHTML.slice(0, -Infinity);
+    }
+}
+addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "1":
+            appendOperation(1);
+            break;
+        case "2":
+            appendOperation(2);
+            break;
+        case "3":
+            appendOperation(3);
+            break;
+        case "4":
+            appendOperation(4);
+            break;
+    }
+});
