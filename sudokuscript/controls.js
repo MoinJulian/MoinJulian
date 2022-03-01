48ef58ba0b265e3af35c554862161aa11a649eeb
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-03-01 21:40:05 +0100

    master

--- sudokuscript/controls.js
+++ sudokuscript/controls.js
@@ -6,23 +6,23 @@ export let delay = 10;
 export function enableControls() {
     $("#clearBtn").click(clearSudoku);
     $("#solveBtn").click(toggleSolve);
-    $("#fasterBtn").click(decreaseDelay);
-    $("#slowerBtn").click(increaseDelay);
+    // $("#fasterBtn").click(decreaseDelay);
+    // $("#slowerBtn").click(increaseDelay);
 }
 
-function decreaseDelay() {
-    if (delay < 10) {
-        delay = 0;
-        $("#fasterBtn").prop("disabled", true);
-    } else {
-        delay /= 2;
-    }
-}
+// function decreaseDelay() {
+//     if (delay < 10) {
+//         delay = 0;
+//         $("#fasterBtn").prop("disabled", true);
+//     } else {
+//         delay /= 2;
+//     }
+// }
 
-function increaseDelay() {
-    delay == 0 ? (delay = 10) : (delay *= 2);
-    $("#fasterBtn").prop("disabled", false);
-}
+// function increaseDelay() {
+//     delay == 0 ? (delay = 10) : (delay *= 2);
+//     $("#fasterBtn").prop("disabled", false);
+// }
 
 function clearSudoku() {
     $(".cell").val("").removeClass("fixed");
