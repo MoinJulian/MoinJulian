1a95dc83b81c63c8de76948348fd64f72544bdbb
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-03-08 07:54:37 +0100

    master

--- jumpandrunjs/levels/level10.js
+++ jumpandrunjs/levels/level10.js
@@ -7,8 +7,7 @@ import { Goal } from "../objects/Goal.js";
 export const level10 = new Level({
     size: [800, 600],
     objects: [
-        new Goal({ pos: [760, 560], size: [40, 40], color:"#eee"}),
-        new Rectangle({ pos: [400, 550], size: [10, 50], color: "#eee"}),
+        
         new Player({ pos: [0, 600], size: [40, 40] }),
     ],
 });
