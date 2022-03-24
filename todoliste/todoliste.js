4379a783e0a2ae0eb8ecc9c3f91366da9004132b
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-03-24 10:13:41 +0100

    master

--- todoliste/todoliste.js
+++ todoliste/todoliste.js
@@ -1,12 +1,69 @@
-let operation = 1;
-let todo = 1;
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
 
-    function ToDo() {
-        todo = document.formular.operation.value;
+const todos = JSON.parse(localStorage.getItem("todos"));
 
-        console.log(todo);
+if (todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.text;
     }
 
-console.log(operation);
-console.log(todo);
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
 
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+
+            updateLS();
+        });
+
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        });
+
+        todosUL.appendChild(todoEl);
+
+        input.value = "";
+
+        updateLS();
+    }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll("li");
+
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
