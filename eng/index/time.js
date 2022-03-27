d1656fe23004c5ceab0b80fd72ddba9d2c5e634a
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-03-27 18:25:06 +0200

    master

--- eng/index/time.js
+++ eng/index/time.js
@@ -3,7 +3,7 @@ elem.addEventListener("click", erzeugeZeitStempel);
 
 function erzeugeZeitStempel() {
   var Zeitstempeltext = document.createTextNode(document.lastModified);
-  var TextZuvor = document.createTextNode("Momentanes Datum: ");
+  var TextZuvor = document.createTextNode("Current Date: ");
   document.getElementById("Absatz").appendChild(TextZuvor);
   document.getElementById("Absatz").appendChild(Zeitstempeltext);
 }
