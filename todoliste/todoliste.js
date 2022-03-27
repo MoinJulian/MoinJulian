359bf7a8e5424274f23842ea26da2c7e8ec3dfdd
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-03-27 15:05:29 +0200

    master

--- todoliste/todoliste.js
+++ todoliste/todoliste.js
@@ -5,65 +5,73 @@ const todosUL = document.getElementById("todos");
 const todos = JSON.parse(localStorage.getItem("todos"));
 
 if (todos) {
-    todos.forEach((todo) => {
-        addTodo(todo);
-    });
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
 }
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    addTodo();
+  addTodo();
 });
 
 function addTodo(todo) {
-    let todoText = input.value;
+  let todoText = input.value;
 
-    if (todo) {
-        todoText = todo.text;
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
     }
 
-    if (todoText) {
-        const todoEl = document.createElement("li");
-        if (todo && todo.completed) {
-            todoEl.classList.add("completed");
-        }
+    todoEl.innerText = todoText;
 
-        todoEl.innerText = todoText;
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
 
-        todoEl.addEventListener("click", () => {
-            todoEl.classList.toggle("completed");
+      updateLS();
+    });
 
-            updateLS();
-        });
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
 
-        todoEl.addEventListener("contextmenu", (e) => {
-            e.preventDefault();
+      todoEl.remove();
 
-            todoEl.remove();
+      updateLS();
+    });
 
-            updateLS();
-        });
+    todoEl.addEventListener("dblclick", (e) => {
+      e.preventDefault();
 
-        todosUL.appendChild(todoEl);
+      todoEl.remove();
 
-        input.value = "";
+      updateLS();
+    });
 
-        updateLS();
-    }
+    todosUL.appendChild(todoEl);
+
+    input.value = "";
+
+    updateLS();
+  }
 }
 
 function updateLS() {
-    const todosEl = document.querySelectorAll("li");
+  const todosEl = document.querySelectorAll("li");
 
-    const todos = [];
+  const todos = [];
 
-    todosEl.forEach((todoEl) => {
-        todos.push({
-            text: todoEl.innerText,
-            completed: todoEl.classList.contains("completed"),
-        });
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
     });
+  });
 
-    localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
