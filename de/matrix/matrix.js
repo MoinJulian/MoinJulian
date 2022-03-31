97053e876a9c819e2ede5ee40753b830974c5cb0
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-03-31 09:17:05 +0200

    master

--- de/matrix/matrix.js
+++ de/matrix/matrix.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let cw = window.innerWidth;
-let ch = window.innerHeight;
+let ch = window.innerHeight - 1;
 
 
 canvas.width = cw;
